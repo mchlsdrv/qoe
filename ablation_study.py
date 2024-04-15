Ablation corrected the types of optimizer and loss in the final file + added number of parameter calculation

ablation_study.py
@@ -5,24 +5,25 @@ from model import run_ablation
 import torch
 
 
-TS = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-DATA_ROOT = pathlib.Path('/Users/mchlsdrv/Desktop/QoE/data/zoom/encrypted_traffic/ablation')
-SAVE_DIR = pathlib.Path(f'/Users/mchlsdrv/Desktop/QoE/data/zoom/encrypted_traffic/ablation_{TS}/outputs')
-os.makedirs(SAVE_DIR)
-
-EPOCHS = [10, 50, 100]
-BATCHES = [16, 32, 64]
-LAYERS = [8, 16, 32, 64]
-UNITS = [8, 16, 32, 64]
-LOSS_FUNCTIONS = [torch.nn.MSELoss]
-OPTIMIZERS = [torch.optim.SGD, torch.optim.Adam, torch.optim.Adamax]
-INITIAL_LEARNING_RATES = [0.001, 0.002, 0.005, 0.008, 0.01]
-FEATURES = ['Bandwidth', 'pps', 'avg time between packets', 'packets length']
-LABELS = ['NIQE', 'Resolution', 'fps']
-
 if __name__ == '__main__':
+    TS = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    ABLATION_ROOT = pathlib.Path('/Users/mchlsdrv/Desktop/QoE/data/zoom/encrypted_traffic/ablation')
+    DATA_ROOT = ABLATION_ROOT / 'cv_5_folds'
+    SAVE_DIR = ABLATION_ROOT / f'outputs_{TS}'
+    os.makedirs(SAVE_DIR)
+
+    EPOCHS = [10, 50, 100]
+    BATCHES = [16, 32, 64]
+    LAYERS = [8, 16, 32, 64]
+    UNITS = [8, 16, 32, 64]
+    LOSS_FUNCTIONS = [torch.nn.MSELoss]
+    OPTIMIZERS = [torch.optim.SGD, torch.optim.Adam, torch.optim.Adamax]
+    INITIAL_LEARNING_RATES = [0.001, 0.002, 0.005, 0.008, 0.01]
+    FEATURES = ['Bandwidth', 'pps', 'avg time between packets', 'packets length']
+    LABELS = ['NIQE', 'Resolution', 'fps']
+
     run_ablation(
-        data_root=DATA_ROOT,
+        test_data_root=DATA_ROOT,
         features=FEATURES,
         labels=LABELS,
         batch_size_numbers=BATCHES,
@@ -32,5 +33,5 @@ if __name__ == '__main__':
         loss_functions=LOSS_FUNCTIONS,
         optimizers=OPTIMIZERS,
         initial_learning_rates=INITIAL_LEARNING_RATES,
-        root_save_dir=SAVE_DIR
+        save_dir=SAVE_DIR
     )

data_division.py
@@ -6,14 +6,14 @@ import pandas as pd
 
 def build_test_datasets(data: pd.DataFrame, n_test_sets, test_set_proportion: float, root_save_dir: pathlib.Path):
     """
-    Divides the data into n_test_sets train-test datasets each with proportion of
+    Divides the cv_5_folds into n_test_sets train-test datasets each with proportion of
     (1-test_set_proportion):test_set_proportion respectively.
 
     * Each test dataset is chosen to not include items from other test sets
 
-    :param data: pandas.DataFrame object containing the data
+    :param data: pandas.DataFrame object containing the cv_5_folds
     :param n_test_sets: Number of test sets to produce
-    :param test_set_proportion: The proportion by which to divide the data
+    :param test_set_proportion: The proportion by which to divide the cv_5_folds
     :param root_save_dir: The location to save the datasets at
     :return: None
     """
@@ -29,7 +29,7 @@ def build_test_datasets(data: pd.DataFrame, n_test_sets, test_set_proportion: fl
     # - Produce the set of indices which may be chosen for test
     valid_test_idxs = np.arange(n_items)
 
-    # - Create a n_test_sets train-test sets
+    # - Create an n_test_sets train-test sets
     for test_set_idx in range(n_test_sets):
         # - Chose randomly n_test_items from the all_idxs
         test_idxs = np.random.choice(all_idxs, n_test_items, replace=False)
@@ -46,7 +46,7 @@ def build_test_datasets(data: pd.DataFrame, n_test_sets, test_set_proportion: fl
         # - Save the train / test datasets
 
         # -- Create a dir for the current test set
-        test_set_save_dir = root_save_dir / f'ablation/test_{test_set_idx}'
+        test_set_save_dir = root_save_dir / f'ablation/test{test_set_idx}'
         os.makedirs(test_set_save_dir, exist_ok=True)
 
         # -- Save teh datasets
@@ -54,9 +54,9 @@ def build_test_datasets(data: pd.DataFrame, n_test_sets, test_set_proportion: fl
         test_data.to_csv(test_set_save_dir / f'test_data.csv', index=False)
 
 
-DATA_PATH = pathlib.Path('/data/zoom/encrypted_traffic/data_no_nan.csv')
+DATA_PATH = pathlib.Path('/cv_5_folds/zoom/encrypted_traffic/data_no_nan.csv')
 DATA_SET = pd.read_csv(DATA_PATH)
-SAVE_DIR = pathlib.Path('/data/zoom/encrypted_traffic')
+SAVE_DIR = pathlib.Path('/cv_5_folds/zoom/encrypted_traffic')
 
 if __name__ == '__main__':
     build_test_datasets(data=DATA_SET, n_test_sets=5, test_set_proportion=0.1, root_save_dir=SAVE_DIR)